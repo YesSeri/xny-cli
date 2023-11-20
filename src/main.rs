e4cae18346580bc4c0315c30ec582739bd92033b
Henrik <henrik.zenkert@gmail.com>
2023-11-20 18:46:39 +0100

    better cli messages

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::error::Error;
 use std::fs::{create_dir_all, File};
 use std::io::{Read, Write};
 use std::path::PathBuf;
@@ -9,23 +10,20 @@ use url::Url;
 /// A cli tool for viewing the documentation for a language.
 /// It uses the excellent learnxinyminutes.com website by Adam Bard.
 /// The src code examples are saved in /var/tmp/x-in-y.
-#[derive(Parser, Default)]
-#[command(about = "View documentation in code form", long_about = None)]
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
 struct Cli {
     /// Language to find documentation for, e.g. rust, python, javascript.
-    #[arg(help = "Language to find documentation for", required_unless_present = "show_languages")]
     language: Option<String>,
 
-    /// You can optionally specify a viewer to view the code in.
-    /// This is useful with e.g. bat. If you specify with arg, you get syntax highlighting.
-    /// If you instead pipe, the code will not be highlighted.
-    #[arg(short, long, help = "Specify the viewer to use, e.g. less, bat, cat", long_help)]
+    /// Specify the viewer to use, e.g. less, bat, cat.
+    #[clap(short, long)]
     viewer: Option<String>,
+
     /// List all available languages to view.
-    #[arg(default_value = "false", short, long, help = "List all languages", long_help)]
+    #[clap(short, long, action)]
     show_languages: bool,
 }
-
 
 #[derive(Debug, Deserialize)]
 struct LinkInfo {
@@ -37,75 +35,87 @@ struct LinkInfo {
 
 const URL_PREFIX: &str = "https://learnxinyminutes.com/";
 
-
 fn get_infos() -> Result<Vec<LinkInfo>, Box<serde_json::Error>> {
     let text = include_str!("../data.json");
     let infos: Vec<LinkInfo> = serde_json::from_str(text)?;
     Ok(infos)
 }
 
-fn init(cli: Cli) -> (String, Option<String>, PathBuf) {
-    let language: String = cli.language.unwrap().to_lowercase();
-    let viewer = cli.viewer;
-
-    let folder = PathBuf::from("/var/tmp/x-in-y");
-    (language, viewer, folder)
-}
-
-fn display_available_languages(info_vec: Vec<LinkInfo>){
+fn display_available_languages(info_vec: Vec<LinkInfo>) {
     let lang_string = show_available_langs(info_vec);
     println!("Available languages:\n{}", lang_string);
 }
 
-fn display_documentation(cli: Cli, info_vec: Vec<LinkInfo>)-> Result<(), Box<dyn std::error::Error>>{
-        let (language, viewer, folder) = init(cli);
-        let info = info_vec.into_iter()
-            .find(|info| *info.name.to_lowercase() == language.to_lowercase())
-            .ok_or("Could not find language")?;
+fn display_documentation(
+    language: &str,
+    viewer: Option<String>,
+    info_vec: Vec<LinkInfo>,
+) -> Result<(), Box<dyn Error>> {
+    let folder = PathBuf::from("/var/tmp/x-in-y");
+    let info = info_vec
+        .into_iter()
+        .find(|info| *info.name.to_lowercase() == language.to_lowercase())
+        .ok_or("Could not find language")?;
 
-        let url = Url::parse(&format!("{}{}", URL_PREFIX, &info.source_code_link))?;
+    let url = Url::parse(&format!("{}{}", URL_PREFIX, &info.source_code_link))?;
 
-        let path_segments = url.path_segments().ok_or("invalid path segments")?;
-        let file_name = path_segments.last().ok_or("invalid path segments")?;
-        let file_path = folder.join(file_name);
-        let credit_path = file_path.with_extension("txt");
+    let path_segments = url.path_segments().ok_or("invalid path segments")?;
+    let file_name = path_segments.last().ok_or("invalid path segments")?;
+    let file_path = folder.join(file_name);
+    let credit_path = file_path.with_extension("txt");
 
-        if !file_path.exists() {
-            create_dir_all(folder)?;
-            let mut file = File::create(&file_path)?;
-            file.write_all(minreq::get(url).send()?.as_str()?.as_bytes())?;
+    if !file_path.exists() {
+        create_dir_all(folder)?;
+        let mut file = File::create(&file_path)?;
+        file.write_all(minreq::get(url).send()?.as_str()?.as_bytes())?;
+    }
+
+    let credit = format!(
+        "Cli written by Henrik Zenkert, henrik.zenkert@gmail.com\nCode: {}\nFull list:{}",
+        info.contributor_text, info.contributor_link
+    );
+
+    if !credit_path.exists() {
+        let mut file = File::create(&credit_path)?;
+        file.write_all(credit.as_bytes())?;
+    }
+
+    match viewer {
+        Some(viewer) if cfg!(target_os = "linux") => run_process(viewer, file_path, credit_path),
+        _ => {
+            print_stdout(file_path, credit).expect("Could not print to stdout.");
         }
-
-        let credit = format!(
-            "Cli written by Henrik Zenkert, henrik.zenkert@gmail.com\nCode: {}\nFull list:{}",
-            info.contributor_text, info.contributor_link
-        );
-
-        if !credit_path.exists() {
-            let mut file = File::create(&credit_path)?;
-            file.write_all(credit.as_bytes())?;
-        }
-
-        match viewer {
-            Some(viewer) if cfg!(target_os = "linux") => { run_process(viewer, file_path, credit_path) }
-            _ => { print_stdout(file_path, credit).expect("Could not print to stdout."); }
-        }
-        Ok(())
+    }
+    Ok(())
 }
-
-fn main() -> Result<(), Box<dyn std::error::Error>> {
+fn main() -> Result<(), Box<dyn Error>> {
     let cli = Cli::parse();
+
     let info_vec = get_infos()?;
+
     if cli.show_languages {
         display_available_languages(info_vec);
+    } else if let Some(language) = cli.language {
+        display_documentation(&language, cli.viewer, info_vec)?;
     } else {
-        display_documentation(cli, info_vec)?;
+        println!(r"error: The following required arguments were not provided:
+    <LANGUAGE>
+USAGE:
+    
+    xny [OPTIONS] LANGUAGE 
+    xny [OPTIONS] --help
+    xny [OPTIONS] --version");
     }
+
     Ok(())
 }
 
 fn show_available_langs(info_vec: Vec<LinkInfo>) -> String {
-    info_vec.into_iter().map(|info| info.name).collect::<Vec<String>>().join("\n")
+    info_vec
+        .into_iter()
+        .map(|info| info.name)
+        .collect::<Vec<String>>()
+        .join("\n")
 }
 
 fn run_process(viewer: String, file_path: PathBuf, credit_path: PathBuf) {
